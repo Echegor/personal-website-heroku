c66a21f483c0ab75f97b5154888dfe0e4d815c18
Luis Echegorri <lme4@njit.edu>
2018-10-15 16:22:51 -0400

    minor

--- index.js
+++ index.js
@@ -6,19 +6,3 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('*', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-//const express = require('express');
-//const path = require('path');
-
-//const app = express();
-
-// Serve only the static files form the dist directory
-
-//app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + index.html));
-});
-
-//var server = app.listen(process.env.PORT || 8080, function () {
-//    var port = server.address().port;
-//    console.log("App now running on port", port);
-//});
