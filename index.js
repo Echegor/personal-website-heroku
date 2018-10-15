8a498a38c9ac3468b81f5518327982209f82ed9e
Luis Echegorri <lme4@njit.edu>
2018-10-15 16:10:42 -0400

    Added home routing

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname));
-console.log(""+__dirname);
-app.get('/*', function(req,res) {
-    
+app.get('/home', function(req,res) {
 res.sendFile(path.join(__dirname + index.html));
 });
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + index.html));
+});
+
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
