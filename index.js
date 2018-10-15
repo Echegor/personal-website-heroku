91de36f327cdb2cb3bb4ed6c863bf84ad4fcac70
Luis Echegorri <lme4@njit.edu>
2018-10-15 15:55:36 -0400

    trying again

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+//const express = require('express')
+//const path = require('path')
+//const PORT = process.env.PORT || 5000
 
-//Install express server
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 const express = require('express');
 const path = require('path');
 
@@ -16,6 +25,3 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
-// Start the app by listening on the default Heroku port
-//app.listen(process.env.PORT || 8080);
-
