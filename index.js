45fe76a8c2e7d813df5b45bd747db18c2377a6ec
Luis Echegorri <lme4@njit.edu>
2018-10-15 16:20:54 -0400

    moved files

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-//const express = require('express')
-//const path = require('path')
-//const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//express()
-//  .use(express.static(path.join(__dirname, 'public')))
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
-//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('*', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const express = require('express');
-const path = require('path');
+//const express = require('express');
+//const path = require('path');
 
-const app = express();
+//const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));
-app.get('/home', function(req,res) {
-res.sendFile(path.join(__dirname + index.html));
-});
-app.get('/*', function(req,res) {
+
+//app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname + index.html));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-});
+//var server = app.listen(process.env.PORT || 8080, function () {
+//    var port = server.address().port;
+//    console.log("App now running on port", port);
+//});
