8f9556438bb2c21b1fd4a946499807bca6891d2e
Luis Echegorri <lme4@njit.edu>
2018-10-16 09:56:16 -0400

    fixed missing path.join arg

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function done(event){
+	console.log("Deployed on port ",PORT)
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/home', (req, res) => res.sendFile(__dirname, 'public','index.html'))
-  .get('*', (req, res) => res.sendFile(__dirname, 'public','index.html'))
+//  .get('/home', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html'),done))
+  .get('*', (req, res) => res.sendFile(path.join(__dirname,'public','index.html'),done))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
